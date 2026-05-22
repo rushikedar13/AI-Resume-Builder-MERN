5ca9e69b2babf625f3eb88224cb2cc6c318cfae0
Rushikesh Kedar <rushikedar1394@gmail.com>
2026-05-22 18:56:56 +0530

    Fix import case for Features component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Banner from "../components/Home/Banner";
 import Hero from "../components/Home/Hero";
-import Features from "../components/Home/features";
+import Features from "../components/Home/Features";
 import CallToAction from "../components/Home/CallToAction";
 import Footer from "../components/Home/Footer";
 
